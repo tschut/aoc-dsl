5aeb3d49af28aaa283b360b66b992b882fa0b85c
Tiemen Schut <tschut@gmail.com>
2023-01-16 20:21:20 +0100

    Add publishing stuff to build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -4,8 +4,9 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.serialization") version "1.7.20"
     `java-library`
-    `maven-publish`
     id("net.researchgate.release") version "3.0.2"
+    `maven-publish`
+    signing
 }
 
 java {
@@ -32,14 +33,55 @@ tasks.getByName<Test>("test") {
 
 publishing {
     publications {
-        create<MavenPublication>(name = "aoc-dsl") {
-            from(components["java"])
-            groupId = "com.github.tschut"
+        create<MavenPublication>("mavenJava") {
             artifactId = "aoc-dsl"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("AoC DSL")
+                description.set("A Kotlin DSL for adventofcode.")
+                url.set("https://github.com/tschut/aoc-dsl")
+                licenses {
+                    license {
+                        name.set("The Unlicense")
+                        url.set("https://unlicense.org")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("tschut")
+                        name.set("Tiemen Schut")
+                        email.set("tschut@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/tschut/aoc-dsl.git")
+                    developerConnection.set("scm:git:https://github.com/tschut/aoc-dsl.git")
+                    url.set("https://github.com/tschut/aoc-dsl")
+                }
+            }
         }
     }
-
     repositories {
-        mavenCentral()
+        maven {
+            name = "OSS"
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials(PasswordCredentials::class)
+        }
     }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
